29c41f66716f1bd6e71e0cf5c9dfc1f9ba781fbe
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-11-10 17:54:20 -0300

    Proyecto MVC agregando modelo

---
+++ MenuRoleMVC.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MenuRoleMVC.WEB.MvcApplication" Language="C#" %>
---
+++ MenuRoleMVC.WEB/Models/LoginModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MenuRoleMVC.WEB.Models
+{
+    public class LoginModels
+    {
+        public int UsuarioId { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese usuario")]
+        public string Usuario { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public int? UserRoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
